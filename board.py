c005905b6d2464dd0ab75971822d441cda90b541
Hridayesh Joshi <hridayeshvjoshi@gmail.com>
2020-07-21 13:00:35 -0700

    board with incomplete methods

--- board.py
+++ board.py
@@ -4,3 +4,20 @@ class Board(object):
     
     def __eq__(self,other):
         return self.values == other.values 
+
+    def boardFull(self):
+        for (x,y) in self.values:
+            if (not x) return False
+        return True
+    
+    def gameWinner(self):
+        for i in range(len(self.values)):
+            for j in range(len(self.values[0])):
+                (piece,player) = self.values[i][j]
+                if(gameWinnerHelper(i,j,piece,player)) return (True,player)
+        return None
+    @staticmethod
+    def gameWinnerHelper(xstart,ystart,piece,player):
+        
+
+
