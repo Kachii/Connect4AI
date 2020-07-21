3ce8c1d5b122018c424b18f588bacbf5df34715c
Kachii <49163980+Kachii@users.noreply.github.com>
2020-07-21 12:33:10 -0700

    Players are updated

--- game.py
+++ game.py
@@ -1,2 +1,8 @@
 from board import *
+from player import *
 gameboard = Board()
+human = Player(False)
+AI = Player(True)
+print("Works!")
+print(human.whatbool())
+print(AI.whatbool())
--- player.py
+++ player.py
@@ -1,6 +1,9 @@
 class Player(object): 
-    def __init__(self):
-        self.values = [[(False,False)]*7]*6
+    def __init__(self, player):
+        self.values = player
     
     def __eq__(self,other):
         return self.values == other.values 
+    
+    def whatbool(self):
+        return self.values
