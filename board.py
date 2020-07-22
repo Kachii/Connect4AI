fbd7f53ab29acddefe6a6e402b7544e5dd81abd9
Hridayesh Joshi <hridayeshvjoshi@gmail.com>
2020-07-22 11:37:53 -0700

    gameOver method incomplete

--- board.py
+++ board.py
@@ -1,6 +1,8 @@
 class Board(object): 
-    def __init__(self):
+    def __init__(self, player1, player2):
         self.values = [[(False,False)]*7]*6
+        self.player1 = player1
+        self.player2 = player2 
     
     def __eq__(self,other):
         return self.values == other.values 
@@ -10,30 +12,38 @@ class Board(object):
     """
     @staticmethod:
     def boardFull(bd):
-        for (x,y) in bd:
-            if (not x) return False
+        for L in bd:
+            for (x,y) in L:
+                if (not x) return False
         return True
     
     @staticmethod 
     def gameWinner(bd):
-        for i in range(len(bd)):
-            for j in range(len(bd[0])):
-                (piece,player) = bd[i][j]
-                if(gameWinnerHelper(i,j,piece,player)) return (True,player)
+        for xstart in range(len(bd)):
+            for ystart in range(len(bd[0])):
+                (piece,player) = bd[xstart][ystart]
+                if(gameWinnerHelper(xstart,ystart,piece,player)!= None) return (True,player)
         return None
 
     @staticmethod
     def gameWinnerHelper(xstart,ystart,piece,player):
-        return None
+        for xdir in [-1,0,1]:
+            for ydir in [-1,0,1]:
+                if(gameWinnerHelperDirection(xstart,ystart,piece,player,xdir,ydir) != None) return (True,player)
+        return None 
     @staticmethod 
     def gameWinnerHelperDirection(xstart,ystart,piece,player,xdir,ydir):
-        return None 
-
+        for i in range(4):
+            X = xstart + i*xdir
+            Y = ystart + i*ydir 
+            (currPiece,currPlayer) = self.values[X][Y]
+            (if X < 0 or  X > 7 or  Y < 0 or  Y > 6): #checking if pieces within bounds of board
+                return None
+            (currPiece,currPlayer) = self.values[X][Y]
+            if((currPiece != piece) or )
     def gameOver(self):
-        if(boardFull(self.values)):
-            return None 
-        else:
-            return gameWinner(self.values)
+        #return gameWinner(self.values)
+         
 
 
 
